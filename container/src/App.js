3228695546e177406eb1306fd16d435fe3682aed
Davi Ribeiro Oliveira <90004943@pmenos.com.br>
2021-12-09 14:27:56 -0300

    Prefixed Class Names

--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
+
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "container-",
+});
 
 export default () => {
   return (
     <div>
-      <h1>Hi There</h1>
-      <MarketingApp />
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
+      </BrowserRouter>
     </div>
   );
 };
